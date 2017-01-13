b728c9d66191fa588edc31189b10b870f244f5eb
Eric Avdey <eiri@eiri.ca>
2017-01-13 14:12:32 -0400

    Add setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name='Nixie',
+  version='0.0.1',
+  description='Counter service',
+  author='Eric Avdey',
+  author_email='eiri@eiri.ca',
+  url='https://github.com/eiri/nixie/',
+  packages=['nixie']
+)
