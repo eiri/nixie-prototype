ee7d9de25eed2591fb89210c0283a8ff26a7ce1a
Eric Avdey <eiri@eiri.ca>
2017-11-01 17:47:03 -0300

    Better CRUD testing

--- tests/test_nixie_crud.py
+++ tests/test_nixie_crud.py
@@ -5,62 +5,71 @@ class NixieCRUDTestCase(unittest.TestCase):
 
   regexp = '[a-f0-9]{32}'
 
-  def test_create_default(self):
+  def test_create(self):
     nx = Nixie()
-    key = nx.create()
-    self.assertRegexpMatches(key, self.regexp)
+    key1 = nx.create()
+    key2 = nx.create()
+    self.assertRegexpMatches(key1, self.regexp)
+    self.assertRegexpMatches(key2, self.regexp)
+    self.assertNotEqual(key1, key2)
 
   def test_create_and_read(self):
     nx = Nixie()
-    key = nx.create()
-    value = nx.read(key)
-    self.assertEqual(value, 0)
+    key1 = nx.create()
+    key2 = nx.create()
+    self.assertEqual(nx.read(key1), 0)
+    self.assertEqual(nx.read(key2), 0)
 
   def test_list(self):
     nx = Nixie()
+    nx.create()
+    nx.create()
     keys = nx.list()
     self.assertIsInstance(keys, list)
     [self.assertRegexpMatches(key, self.regexp) for key in keys]
 
-  def test_update(self):
+  def test_update_separate(self):
     nx = Nixie()
-    key = nx.create()
-    value = nx.update(key)
-    self.assertEqual(value, 1)
-    value = nx.update(key)
-    self.assertEqual(value, 2)
+    key1 = nx.create()
+    key2 = nx.create()
+    self.assertEqual(nx.update(key1), 1)
+    self.assertEqual(nx.update(key1), 2)
+    self.assertEqual(nx.read(key2), 0)
+    self.assertEqual(nx.update(key2), 1)
+    self.assertEqual(nx.update(key2), 2)
+    self.assertEqual(nx.update(key2), 3)
+    self.assertEqual(nx.read(key1), 2)
 
   def test_update_with_value(self):
     nx = Nixie()
-    key = nx.create()
-    value = nx.update(key, 4)
-    self.assertEqual(value, 4)
-    value = nx.update(key, 12)
-    self.assertEqual(value, 16)
+    key1 = nx.create()
+    key2 = nx.create()
+    self.assertEqual(nx.update(key1, 4), 4)
+    self.assertEqual(nx.update(key1, 8), 12)
+    self.assertEqual(nx.read(key2), 0)
+    self.assertEqual(nx.update(key2, 5), 5)
+    self.assertEqual(nx.update(key2, 10), 15)
+    self.assertEqual(nx.read(key1), 12)
 
   def test_update_with_negative_value(self):
     nx = Nixie()
     key = nx.create()
-    value = nx.update(key, -5)
-    self.assertEqual(value, -5)
+    self.assertEqual(nx.update(key, -5), -5)
 
   def test_exists(self):
     nx = Nixie()
-    key = nx.create()
-    exists = nx.exists(key)
-    self.assertTrue(exists)
-
-  def test_exists_missing(self):
-    nx = Nixie()
-    exists = nx.exists('missing_counter')
-    self.assertFalse(exists)
+    key1 = nx.create()
+    key2 = nx.create()
+    self.assertTrue(nx.exists(key1))
+    self.assertTrue(nx.exists(key2))
+    self.assertFalse(nx.exists('missing_counter'))
 
   def test_delete(self):
     nx = Nixie()
-    key = nx.create()
-    exists = nx.exists(key)
-    self.assertTrue(exists)
-    delete_result = nx.delete(key)
-    exists_after = nx.exists(key)
-    self.assertTrue(delete_result)
-    self.assertFalse(exists_after)
+    key1 = nx.create()
+    key2 = nx.create()
+    self.assertTrue(nx.delete(key1))
+    self.assertFalse(nx.exists(key1))
+    self.assertTrue(nx.exists(key2))
+    self.assertTrue(nx.delete(key2))
+    self.assertFalse(nx.exists(key2))
