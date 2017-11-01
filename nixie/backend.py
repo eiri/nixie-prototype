be90c119976357373316456c583b4959630c0135
Eric Avdey <eiri@eiri.ca>
2017-11-01 17:24:15 -0300

    Inherit backend from dict

--- nixie/backend.py
+++ nixie/backend.py
@@ -2,25 +2,22 @@
 Dict storage backend
 """
 
-class Backend():
+class Backend(dict):
 
-  def __init__(self):
-    self.storage = {}
+  def __init__(self, *arg, **kwarg):
+    super(Backend, self).__init__(*arg, **kwarg)
 
   def get(self, key):
-    return int(self.storage[key]) if key in self.storage else None
+    return int(self[key]) if key in self else None
 
   def set(self, key, value):
-    self.storage[key] = int(value)
+    self[key] = int(value)
     return True
 
   def remove(self, key):
-    if key in self.storage:
-      del self.storage[key]
+    if key in self:
+      del self[key]
     return True
 
   def has(self, key):
-    return key in self.storage
-
-  def keys(self):
-    return self.storage.keys()
+    return key in self
