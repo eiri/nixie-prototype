864642a1788003d8c11dc5feb4422c524bb9722e
Eric Avdey <eiri@eiri.ca>
2021-04-22 00:42:23 -0300

    Swith to HTTPMethodView and simplify HTTP API

--- nixie/frontend.py
+++ nixie/frontend.py
@@ -1,54 +1,52 @@
 from sanic import Sanic, exceptions
 from sanic.response import text, empty
+from sanic.views import HTTPMethodView
+
 from nixie.core import Nixie, KeyError
 
-class Frontend:
-  """REST frontend"""
+class NixieRootView(HTTPMethodView):
+  """View for Nixie root end-point"""
 
-  def __init__(self):
-    self.nx = Nixie()
-    self.app = Sanic(__name__.split('.')[0])
-    self.app.config.FALLBACK_ERROR_FORMAT = "text"
-    self.__set_rules()
-    return None
+  def __init__(self, nx):
+    self.nx = nx
+    super().__init__()
 
-  def run(self, port=7312, debug=False):
-    self.app.run(port=port, debug=debug)
+  async def get(self, request):
+    keys = self.nx.list()
+    resp =  '\n'.join(keys)
+    return text(resp)
 
-  def __set_rules(self):
-    self.app.add_route(self.list, '/', methods=['GET'])
-    self.app.add_route(self.create, '/', methods=['POST'], )
-    self.app.add_route(self.exists, '/<key>', methods=['HEAD'])
-    self.app.add_route(self.read, '/<key>', methods=['GET'])
-    # self.app.add_route(self.put, '/<key:string>/<val:int>', methods=['PUT'])
-    self.app.add_route(self.incr, '/<key>/incr', methods=['PUT'])
-    self.app.add_route(self.decr, '/<key>/decr', methods=['PUT'])
-    self.app.add_route(self.update, '/<key>/incr/<val:int>', methods=['PUT'])
-    self.app.add_route(self.update, '/<key>/decr/<val:int>', methods=['PUT'])
-    self.app.add_route(self.delete, '/<key>', methods=['DELETE'])
-
-  """CRUD"""
-  async def create(self, request):
+  async def post(self, request):
     key = self.nx.create()
     return text(key, status=201)
 
-  async def read(self, request, key):
+
+class NixieCounterView(HTTPMethodView):
+  """View for Nixie counter end-point"""
+
+  def __init__(self, nx):
+    self.nx = nx
+    super().__init__()
+
+  async def get(self, request, key):
     val = self.nx.read(key)
     if val is None:
       raise exceptions.NotFound(f'Not Found')
     else:
       return text(f'{val}')
 
-  async def update(self, request, key, val):
+  async def head(self, request, key):
+    if self.nx.exists(key):
+      return empty()
+    else:
+      raise exceptions.NotFound(f'Not Found')
+
+  async def post(self, request, key):
     try:
-      if 'decr' in request.path:
-        val = -1 * val
-      new_val = self.nx.update(key, val)
+      new_val = self.nx.incr(key)
       return text(f'{new_val}')
     except KeyError as e:
       raise exceptions.NotFound(f'{e}')
-    except ValueError as e:
-      raise exceptions.InvalidUsage(f'{e}')
 
   async def delete(self, request, key):
     try:
@@ -57,37 +55,17 @@ class Frontend:
     except KeyError as e:
       raise exceptions.NotFound(f'{e}')
 
-  """extra"""
-  async def exists(self, request, key):
-    if self.nx.exists(key):
-      return empty()
-    else:
-      raise exceptions.NotFound(f'Not Found')
 
-  async def list(self, request):
-    keys = self.nx.list()
-    resp =  '\n'.join(keys)
-    return text(resp)
+class Frontend:
+  """REST frontend"""
 
-  async def put(self, request, key, val):
-    try:
-      new_val = self.nx.put(key, val)
-      return text(f'{new_val}')
-    except KeyError as e:
-      raise exceptions.NotFound(f'{e}')
-    except ValueError as e:
-      raise exceptions.InvalidUsage(f'{e}')
+  def __init__(self):
+    self.nx = Nixie()
+    self.app = Sanic(__name__.split('.')[0])
+    self.app.config.FALLBACK_ERROR_FORMAT = "text"
+    self.app.add_route(NixieRootView.as_view(self.nx), "/")
+    self.app.add_route(NixieCounterView.as_view(self.nx), "/<key:string>")
+    return None
 
-  async def incr(self, request, key):
-    try:
-      new_val = self.nx.incr(key)
-      return text(f'{new_val}')
-    except KeyError as e:
-      raise exceptions.NotFound(f'{e}')
-
-  async def decr(self, request, key):
-    try:
-      new_val = self.nx.decr(key)
-      return text(f'{new_val}')
-    except KeyError as e:
-      raise exceptions.NotFound(f'{e}')
+  def run(self, port=7312, debug=False):
+    self.app.run(port=port, debug=debug)
--- tests/test_frontend_crud.py
+++ tests/test_frontend_crud.py
@@ -9,11 +9,25 @@ class FrontendTestCase(unittest.TestCase):
     fe = Frontend()
     self.app = fe.app.test_client
 
-  def test_create(self):
-    req, resp = self.app.post('/')
-    self.assertEqual(resp.status, 201)
+  def test_empty_list(self):
+    req, resp = self.app.get('/')
+    self.assertEqual(resp.status, 200)
     self.assertTrue(resp.headers['Content-Type'].startswith('text/plain'))
-    self.assertRegex(resp.body, self.regexp)
+    self.assertFalse(resp.body)
+
+  def test_create(self):
+    for _ in range(5):
+        req, resp = self.app.post('/')
+        self.assertEqual(resp.status, 201)
+        self.assertTrue(resp.headers['Content-Type'].startswith('text/plain'))
+        self.assertRegex(resp.body, self.regexp)
+    req, resp = self.app.get('/')
+    self.assertEqual(resp.status, 200)
+    self.assertTrue(resp.headers['Content-Type'].startswith('text/plain'))
+    keys = resp.body.split()
+    self.assertEqual(len(keys), 5)
+    for key in keys:
+      self.assertRegex(key, self.regexp)
 
   def test_read(self):
     key = self.get_key()
@@ -23,30 +37,42 @@ class FrontendTestCase(unittest.TestCase):
     self.assertTrue(resp.headers['Content-Type'].startswith('text/plain'))
     self.assertEqual(resp.body, b'0')
 
-  def test_update_incr(self):
-    key = self.get_key()
-    url = '/{key}/incr/12'.format(key=key)
-    req, resp = self.app.put(url)
-    self.assertEqual(resp.status, 200)
+  def test_read_missing(self):
+    req, resp = self.app.get('/nokey')
+    self.assertEqual(resp.status, 404)
     self.assertTrue(resp.headers['Content-Type'].startswith('text/plain'))
-    self.assertEqual(resp.body, b'12')
-    req, resp = self.app.put(url)
-    self.assertEqual(resp.status, 200)
-    self.assertTrue(resp.headers['Content-Type'].startswith('text/plain'))
-    self.assertEqual(resp.body, b'24')
+    self.assertIn(b'Not Found', resp.body)
 
-  def test_update_decr(self):
+  def test_exists(self):
     key = self.get_key()
-    req, resp = self.app.put('/{key}/incr/17'.format(key=key))
-    url = '/{key}/decr/6'.format(key=key)
-    req, resp = self.app.put(url)
+    url = '/{key}'.format(key=key)
+    req, resp = self.app.head(url)
+    self.assertEqual(resp.status, 204)
+    self.assertFalse(resp.body)
+
+  def test_not_exists(self):
+    req, resp = self.app.head('/nokey')
+    self.assertEqual(resp.status, 404)
+    self.assertTrue(resp.headers['Content-Type'].startswith('text/plain'))
+    self.assertFalse(resp.body)
+
+  def test_update(self):
+    key = self.get_key()
+    url = '/{key}'.format(key=key)
+    req, resp = self.app.post(url)
     self.assertEqual(resp.status, 200)
     self.assertTrue(resp.headers['Content-Type'].startswith('text/plain'))
-    self.assertEqual(resp.body, b'11')
-    req, resp = self.app.put(url)
+    self.assertEqual(resp.body, b'1')
+    req, resp = self.app.post(url)
     self.assertEqual(resp.status, 200)
     self.assertTrue(resp.headers['Content-Type'].startswith('text/plain'))
-    self.assertEqual(resp.body, b'5')
+    self.assertEqual(resp.body, b'2')
+
+  def test_update_missing(self):
+    req, resp = self.app.post('/nokey')
+    self.assertEqual(resp.status, 404)
+    self.assertTrue(resp.headers['Content-Type'].startswith('text/plain'))
+    self.assertIn(b'Not Found', resp.body)
 
   def test_delete(self):
     key = self.get_key()
@@ -54,6 +80,12 @@ class FrontendTestCase(unittest.TestCase):
     req, resp = self.app.delete(url)
     self.assertEqual(resp.status, 204)
     self.assertFalse(resp.body)
+
+  def test_delete_missing(self):
+    req, resp = self.app.delete('/nokey')
+    self.assertEqual(resp.status, 404)
+    self.assertTrue(resp.headers['Content-Type'].startswith('text/plain'))
+    self.assertIn(b'Not Found', resp.body)
 
   def get_key(self):
     self.app.post('/')
