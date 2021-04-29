9f19fcabd1ecb9d7f848fc7f51489c5e9bf83c03
Eric Avdey <eiri@eiri.ca>
2021-04-28 23:25:28 -0300

    Add counter meta management to API

--- nixie/api.py
+++ nixie/api.py
@@ -7,8 +7,12 @@ class Nixie:
     self.storage = backend.Backend()
 
   """CRUD"""
-  def create(self):
-    key = self.storage.new()
+  def create(self, start=0, step=1, name=None, description=None):
+    key = self.storage.new(start, step)
+    if name is not None:
+      self.storage.counter(key).name = name
+    if description is not None:
+      self.storage.counter(key).description = description
     return key
 
   def read(self, key):
@@ -34,11 +38,34 @@ class Nixie:
     return self.storage.keys()
 
   def next(self, key):
-    if not key in self.storage:
-      raise KeyError('Unknown key {}'.format(key))
+    self.__validate_key(key)
     i = iter(self.storage.counter(key))
     next(i)
     return self.storage[key]
+
+  def read_meta(self, key):
+    self.__validate_key(key)
+    c = self.storage.counter(key)
+    return {
+      "step": c.step,
+      "name": c.name,
+      "description": c.description
+    }
+
+  def update_meta(self, key, step=None, name=None, description=None):
+    self.__validate_key(key)
+    c = self.storage.counter(key)
+    if isinstance(step, int) and step != c.step:
+      c.step = step
+    if name is not None and name != c.name:
+      c.name = name
+    if description is not None and description != c.name:
+      c.description = description
+    return True
+
+  def __validate_key(self, key):
+    if not key in self.storage:
+      raise KeyError('Unknown key {}'.format(key))
 
   def __validate_key_value(self, key, value):
     if not key in self.storage:
--- tests/test_api.py
+++ tests/test_api.py
@@ -50,9 +50,17 @@ class APITestCase(unittest.TestCase):
     with self.assertRaises(KeyError):
       self.nx.delete('missing')
 
+  def test_crud_none_default(self):
+    key3 = self.nx.create(3, 3, "key3", "description of key3")
+    self.assertRegex(key3, self.regexp)
+    self.assertEqual(self.nx.read(key3), 3)
+    self.assertEqual(self.nx.next(key3), 6)
+    meta3 = self.nx.read_meta(key3)
+    self.assertEqual(meta3['step'], 3)
+    self.assertEqual(meta3['name'], 'key3')
+    self.assertEqual(meta3['description'], 'description of key3')
+
   def test_list(self):
-    self.nx.create()
-    self.nx.create()
     for key in self.nx.list():
       self.assertRegex(key, self.regexp)
 
@@ -68,3 +76,34 @@ class APITestCase(unittest.TestCase):
     self.assertEqual(self.nx.read(self.key2), 0)
     self.assertEqual(self.nx.next(self.key2), 1)
     self.assertEqual(self.nx.read(self.key2), 1)
+
+  def test_read_meta_default(self):
+    meta1 = self.nx.read_meta(self.key1)
+    self.assertEqual(meta1['step'], 1)
+    self.assertIsNone(meta1['name'])
+    self.assertIsNone(meta1['description'])
+
+  def test_read_meta_missing(self):
+    with self.assertRaises(KeyError):
+      self.nx.read_meta('missing')
+
+  def test_update_meta(self):
+    self.nx.update_meta(self.key1, 2)
+    meta1 = self.nx.read_meta(self.key1)
+    self.assertEqual(meta1['step'], 2)
+    self.assertIsNone(meta1['name'])
+    self.assertIsNone(meta1['description'])
+    self.nx.update_meta(self.key1, name='new name')
+    meta1 = self.nx.read_meta(self.key1)
+    self.assertEqual(meta1['step'], 2)
+    self.assertEqual(meta1['name'], 'new name')
+    self.assertIsNone(meta1['description'])
+    self.nx.update_meta(self.key1, description='new description')
+    meta1 = self.nx.read_meta(self.key1)
+    self.assertEqual(meta1['step'], 2)
+    self.assertEqual(meta1['name'], 'new name')
+    self.assertEqual(meta1['description'], 'new description')
+    meta2 = self.nx.read_meta(self.key2)
+    self.assertEqual(meta2['step'], 1)
+    self.assertIsNone(meta2['name'])
+    self.assertIsNone(meta2['description'])
